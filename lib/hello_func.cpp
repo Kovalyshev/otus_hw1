db850677c7d5b4cee74b7a71e26c922f304c582c
dev <dev@services.services>
2019-04-23 11:09:59 -0500

    Попытка собрать#6

--- lib/hello_func.cpp
+++ lib/hello_func.cpp
@@ -4,5 +4,5 @@
 using namespace std;
 
 string fullVersion() {
-	return to_string(PROJECT_VERSION_MAJOR) + "." + to_string(PROJECT_VERSION_MINOR) + "." + to_string(PROJECT_VERSION_PATCH);
+	return str(PROJECT_VERSION_MAJOR) + "." + str(PROJECT_VERSION_MINOR) + "." + str(PROJECT_VERSION_PATCH);
 }
