2feed58417c41de86bee4794979e2fe76ea87b47
dev <dev@services.services>
2019-04-23 11:14:26 -0500

    Попытка собрать#7

--- lib/hello_func.cpp
+++ lib/hello_func.cpp
@@ -1,8 +1,12 @@
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "hello_func.h"
 #include "version.h"
 
 using namespace std;
 
 string fullVersion() {
-	return str(PROJECT_VERSION_MAJOR) + "." + str(PROJECT_VERSION_MINOR) + "." + str(PROJECT_VERSION_PATCH);
+	return to_string(PROJECT_VERSION_MAJOR) + "." + to_string(PROJECT_VERSION_MINOR) + "." + to_string(PROJECT_VERSION_PATCH);
 }
