60e541a25f311ee124e5eb7cd00fbd54224eac57
dev <dev@services.services>
2019-04-24 10:53:59 -0500

    Попытка собрать#9

--- lib/hello_func.cpp
+++ lib/hello_func.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,5 +9,7 @@
 using namespace std;
 
 string fullVersion() {
-	return to_string(PROJECT_VERSION_MAJOR) + "." + to_string(PROJECT_VERSION_MINOR) + "." + to_string(PROJECT_VERSION_PATCH);
+	ostringstream stream;
+	stream << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR << "." << PROJECT_VERSION_PATCH;
+	return stream.str();
 }
